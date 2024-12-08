a969ccd1aa50cbe98d0416175fbe23d83a5ff0e7
HashimotoShuhei-z <hassii0510@outlook.jp>
2024-12-08 22:57:31 +0900

    eslintの修正

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -1,13 +1,15 @@
-"use client"
+'use client'
 
-import type { IconButtonProps } from "@chakra-ui/react"
-import { ClientOnly, IconButton, Skeleton } from "@chakra-ui/react"
-import { ThemeProvider, useTheme } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
-import * as React from "react"
-import { LuMoon, LuSun } from "react-icons/lu"
+import type { IconButtonProps } from '@chakra-ui/react'
+import { ClientOnly, IconButton, Skeleton } from '@chakra-ui/react'
+import { ThemeProvider, useTheme } from 'next-themes'
+import type { ThemeProviderProps } from 'next-themes'
+import * as React from 'react'
+import { LuMoon, LuSun } from 'react-icons/lu'
 
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 export interface ColorModeProviderProps extends ThemeProviderProps {}
+/* eslint-enable @typescript-eslint/no-empty-object-type */
 
 export function ColorModeProvider(props: ColorModeProviderProps) {
   return (
@@ -18,7 +20,7 @@ export function ColorModeProvider(props: ColorModeProviderProps) {
 export function useColorMode() {
   const { resolvedTheme, setTheme } = useTheme()
   const toggleColorMode = () => {
-    setTheme(resolvedTheme === "light" ? "dark" : "light")
+    setTheme(resolvedTheme === 'light' ? 'dark' : 'light')
   }
   return {
     colorMode: resolvedTheme,
@@ -29,15 +31,17 @@ export function useColorMode() {
 
 export function useColorModeValue<T>(light: T, dark: T) {
   const { colorMode } = useColorMode()
-  return colorMode === "light" ? light : dark
+  return colorMode === 'light' ? light : dark
 }
 
 export function ColorModeIcon() {
   const { colorMode } = useColorMode()
-  return colorMode === "light" ? <LuSun /> : <LuMoon />
+  return colorMode === 'light' ? <LuSun /> : <LuMoon />
 }
 
-interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+interface ColorModeButtonProps extends Omit<IconButtonProps, 'aria-label'> {}
+/* eslint-enable @typescript-eslint/no-empty-object-type */
 
 export const ColorModeButton = React.forwardRef<
   HTMLButtonElement,
@@ -55,8 +59,8 @@ export const ColorModeButton = React.forwardRef<
         {...props}
         css={{
           _icon: {
-            width: "5",
-            height: "5",
+            width: '5',
+            height: '5',
           },
         }}
       >
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Routes } from 'react-router-dom'
 
 export const Router = () => {
